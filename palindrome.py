0cbb5f3c502b595879b7ae0c7252b7e223f1b871
jeff <jeffersonhvuong@gmail.com>
2022-09-12 14:45:45 -0400

    first commit

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        def stringint(self):
+            if x >= 0 and x < 10:
+                
+            pali = str(x)
+            lenpali = len(x)
+            n = lenpali - 1
+            for x in range(len(pali)):
+                x = 1
