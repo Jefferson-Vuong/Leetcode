a2ac331ac102908e150326762edca2de4af07830
jeff <jeffersonhvuong@gmail.com>
2022-09-12 14:53:53 -0400

    Negative and 0 case

--- palindrome.py
+++ palindrome.py
@@ -2,8 +2,10 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         
         def stringint(self):
-            if x >= 0 and x < 10:
-                
+            if x < 0:
+                return False
+            if x == 0:
+                return True
             pali = str(x)
             lenpali = len(x)
             n = lenpali - 1
